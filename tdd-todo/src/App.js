5be318b3bc9cb8ad934752aa16bb933d995e57e3
OseungKwon <00osos@naver.com>
2021-09-07 23:20:48 +0900

    update

--- tdd-todo/src/App.js
+++ tdd-todo/src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "test1",
+      done: true
+    },
+    { id: 2, text: "test2", done: false }
+  ]);
+  const nextId = useRef(3);
+  const onInsert = (text) => {
+    setTodos(
+      todos.concat({
+        id: nextId.current,
+        text,
+        done: false
+      })
+    );
+    nextId.current += 1;
+  };
+  const onToggle = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
   return (
     <div>
-      <TodoForm data-testid="helloword" />
-      <TodoList todos={[]} />
+      <TodoForm onInsert={onInsert} />
+      <TodoList todos={todos} onToggle={onToggle} />
     </div>
   );
 };
--- tdd-todo/src/App.test.js
+++ tdd-todo/src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import App from "./App";
 
 describe("<App/>", () => {
@@ -7,5 +7,29 @@ describe("<App/>", () => {
     const { getByText, getByTestId } = render(<App />);
     getByText("등록");
     getByTestId("TodoList");
+  });
+  it("renders two defaults todos", () => {
+    const { getByText } = render(<App />);
+    getByText("test1");
+    getByText("test2");
+  });
+  it("creates new todo", () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+    fireEvent.change(getByPlaceholderText("할 일을 입력하세요"), {
+      target: {
+        value: "새 항목 추가하기"
+      }
+    });
+    fireEvent.click(getByText("등록"));
+    getByText("새 항목 추가하기");
+  });
+  it("toggles todo", () => {
+    const { getByText } = render(<App />);
+    const todoText = getByText("test1");
+    expect(todoText).toHaveStyle("text-decoration: line-through");
+    fireEvent.click(todoText);
+    expect(todoText).not.toHaveStyle("text-decoration: line-through");
+    fireEvent.click(todoText);
+    expect(todoText).toHaveStyle("text-decoration: line-through");
   });
 });
